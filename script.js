40211024eac50cf9545a0f2704c6a3cda942eab5
Om Ahirvar <158020362+omahirwar@users.noreply.github.com>
2026-02-08 19:18:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
+let balance = 0;
+
 function toggleTheme() {
   document.body.classList.toggle("light");
   document.body.classList.toggle("dark");
 }
 
-function copy() {
-  const t = document.getElementById("addr").innerText;
-  navigator.clipboard.writeText(t);
-  alert("Address copied");
+function copyAddr() {
+  navigator.clipboard.writeText(
+    document.getElementById("walletAddr").innerText
+  );
+  alert("Wallet address copied");
+}
+
+function deposit() {
+  const amt = parseFloat(document.getElementById("amount").value);
+  if (!amt || amt <= 0) {
+    alert("Enter valid amount");
+    return;
+  }
+
+  balance += amt;
+  document.getElementById("balance").innerText = balance.toFixed(2);
+  document.getElementById("amount").value = "";
 }
